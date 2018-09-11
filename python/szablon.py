f852790b04b7adbad6aa89968f015949b615846c
gecio19 <dageron92@interia.eu>
2018-09-11 12:21:06 +0200

    Dodanie szablonu Pythona

---
+++ python/szablon.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+#  
+
+def main(args):
+    print("Witam") 
+    
+    
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
