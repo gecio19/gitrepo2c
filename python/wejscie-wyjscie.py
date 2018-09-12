89b9b039d8de823ddb4a22f234775a927c088858
gecio19 <dageron92@interia.eu>
2018-09-12 08:46:29 +0200

    dopisanie funkcji

---
+++ python/wejscie-wyjscie.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+#  
+
+def main(args):
+    a = input("Podaj 1. liczbę: ") 
+    b = input ("Podaj 2. liczbę: ")  
+    
+    print(a) 
+    print (b)
+    
+    
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
