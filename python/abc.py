475d5b1ea89ab50623b02eeddbc03ed46f29ef58
gecio19 <dageron92@interia.eu>
2018-09-20 07:54:59 +0200

    Pętla for i if

---
+++ python/abc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  abc.py
+#  
+#  
+#  
+
+
+def main(args):
+    a = int(input("Podaj 1. liczbę: ")) 
+    b = int(input ("Podaj 2. liczbę: "))
+    c = int(input ("Podaj 3. liczbę: "))
+    
+    
+    
+    
+    if a > b:
+     if a > c:
+        print("Najwiekszą cyfrą jest" ,a)
+    else: 
+        
+        print("Liczby największą będzie" ,c)
+    
+    
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
