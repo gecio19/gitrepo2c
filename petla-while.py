62facf7ca8e619fce112ca5741ccc86de0cf49d6
gecio19 <dageron92@interia.eu>
2018-09-25 12:34:39 +0200

    Petle

--- petla-while.py
+++ petla-while.py
@@ -9,13 +9,19 @@
 def main(args):
     
     start = int(input("Podaj 1. liczbę: ")) 
-    stop = int(input("Podaj 1. liczbę: ")) 
+    stop = int(input("Podaj 2. liczbę: ")) 
     
-    if start>= stop:
+    while start >= stop: #Jeżeli start jest większy od stop pwowiedz "Bład" i pobierz ponownie drugą liczbę do skutku s
+      print("Błąd")
+      stop = int(input("Podaj 2. liczbę: ")) 
+  
+      
+    
+    if start>= stop: #Bierze liczbę1 podaną  
         print("Błędnę dane!")
         exit(0)
     for i in range(start, stop + 1):
-        if i % 2 == 0:
+        if i % 2 == 0: # Jeżeli liczba i jest parzysta drukuję ją 
             print(i)
         else:
             print("Liczba nieparzysta")
