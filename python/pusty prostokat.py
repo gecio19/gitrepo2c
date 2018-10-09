04df6338b90f04df64c01d3a09454fd76f3faa81
gecio19 <dageron92@interia.eu>
2018-10-09 12:36:07 +0200

    choinki

--- python/pusty prostokat.py
+++ python/pusty prostokat.py
@@ -2,19 +2,44 @@
 # -*- coding: utf-8 -*-
 #
 #  pusty prostokat.py
-
-
+def prostokat(a, b, znak):
+    for i in range(a):
+        for j in range(b):
+            print(znak, end='')
+        print()
+        
+        
+def prostokat2(a, b, znak, znak2):
+    for i in range(a):
+        for j in range(b):
+            if j == 0 or j == b - 1 or i == 0 or i == a - 1:
+                print(znak, end='')
+            else:
+                print(znak2, end='')
+        print()
+        
+        
+def choinka(h, znak):
+        for j in range(h):
+            print(znak, end='')
+            
+        print()
+        
 
 def main(args):
     
-    m = int(input("Podaj pierwszy wymiar:"))
-    n = int(input("Podaj drugi wymiar:"))
-    z = (input("Podaj pierwszy wymiar:"))
-    for i in range(m):
-        for j in range(n):
-            print(z, end='')
-        print()
+    a = int(input("Podaj pierwszy wymiar:"))
+    b = int(input("Podaj drugi wymiar:"))
+    h = int(input("Podaj wysokośc choinki"))
+    znak = (input("Podaj pierwszy znak:"))
+    znak2 = (input("Podaj drugi znak:"))
+    znak3 = (input("Podaj choinkowy znak:"))
     
+    prostokat(a, b, znak)
+    print(" ")  
+    prostokat2(a, b, znak, znak2)
+    pirnt(" ")
+    choinka(h, znak)
     
     return 0
 
