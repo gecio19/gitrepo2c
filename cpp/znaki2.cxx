756a10135aa33d4d575b448dd70c64bac3e9bfd5
gecio19 <dageron92@interia.eu>
2018-11-08 07:56:09 +0100

    ascii

--- cpp/znaki2.cxx
+++ cpp/znaki2.cxx
@@ -28,23 +28,66 @@ void drukuj(char tab[], int rozmiar) {
         
         } 
 }
+
+
+void ascii(char tab[], int rozmiar) {
+    int kod = 0; // kodascii znaku
+        
+    
+    for(int i=0; i< rozmiar; i++) {
+        kod = (int)tab[i];
+        if (kod > 96 && kod < 123)
+        cout << (char)(kod - 32) << " ";
+    else
+        cout << (char)(kod + 32) << " ";
+        
+        } 
+}
+
+
+
+
 void liczznaki(char tab[], int rozmiar){
     int spacje = 0;
     int interpunkcja = 0;
     int symbole = 0;
+    int reszta = 0;
     for(int i=0; i< rozmiar; i++){
         
-        if (tab[i] == ' ' ) spacje++;
-        else if (tab[i] == '.' || tab[i] == ',')
-          interpunkcja++
-           else if (tab[i] == '.' || tab[i] == ',')
-          interpunkcja++
+        //~if (tab[i] == ' ' ) spacje++;
+        //~else if (tab[i] == '.' || tab[i] == ',')
+          //~interpunkcja++
+           //~else if (tab[i] == '.' || tab[i] == ',')
+          //~interpunkcja++
+          switch (tab[i]) {
+              case ' ':
+              case '\t':
+                spacje++;
+              break;
+              case '.':
+              case ',':
+                    interpunkcja++; 
+              break;
+              case ')':
+              case '(':
+                    symbole++;
+             break;
+             default:
+                    reszta++;
+                
+              
+              }
           // todo :instrrukcja switch
         
         
         }
+            cout << "Spacje:" << spacje << endl;
+            cout << "interpunkcja:" << interpunkcja << endl;
+                cout << "Symbole:" << symbole << endl;
+        cout << "Reszta:" << reszta << endl;
+
     
-    
+        
     
     }
 
@@ -56,7 +99,8 @@ int main(int argc, char **argv)
 	// cin >> tab;
     cin.getline(tab, rozmiar);
     //~cout << " Cześć " << tab << "!" << endl;
-	drukuj(tab, zlicz(tab));
+	ascii(tab, zlicz(tab));
+    liczznaki(tab, zlicz(tab));
     
 	return 0;
 }
