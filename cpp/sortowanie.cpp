bb3132a28d934531157db3e9706c25849f7abae2
gecio19 <dageron92@interia.eu>
2018-12-13 07:56:11 +0100

    sortowanie

--- cpp/sortowanie.cpp
+++ cpp/sortowanie.cpp
@@ -12,7 +12,7 @@ using namespace std;
 #include <cstdlib>
 
 void wypelnij(int tab[], int roz) {
-    srand(time(NULL)); 
+    srand(time(NULL)); // inicjacja generatora liczb pseudolosowych 
     
     cout << "Wprowadź rozmiar" << roz << "liczb: " << endl;
     for(int i=0; i<roz; i++) {
@@ -31,11 +31,49 @@ void drukuj(int tab[], int roz) {
 }
 
 
+void zamien1(int a, int b) {
+    cout << a << " " << b << endl;
+    int tmp =a;
+    a = b;
+    b = tmp;
+    
+   
+    
+    
+    
+    }
+
+
+
+
+void sort_bubble(int tab[], int n) {
+    
+    for (int j = n - 1; j > 0; j--) 
+        {
+        cout << j << " " << endl;
+        for(int i= 0; i < j; i++)   
+           if (tab[i] > tab[i + 1])
+                zamien1(tab[i], tab[i+1]);
+        
+        
+        
+        }
+        
+    
+    
+    }
+
+
 int main(int argc, char **argv)
 {
-    
-    
-	
+    int roz = 20;
+    int tab[roz];
+    wypelnij(tab, roz);
+    drukuj(tab, roz);
+    cout << endl;
+    //sort_bubble(tab, roz);
+    zamien1(5, 10);
+	cout << endl;
 	return 0;
 }
 
